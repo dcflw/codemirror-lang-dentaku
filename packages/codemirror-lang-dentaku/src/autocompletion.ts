76f97695db9bebc1a0807cdbfa0c1cb06d9b634d
Ocheben <ocheben@gmail.com>
2023-12-18 21:20:47 +0100

    feat: enable duration

--- packages/codemirror-lang-dentaku/src/autocompletion.ts
+++ packages/codemirror-lang-dentaku/src/autocompletion.ts
@@ -30,6 +30,15 @@ export interface DentakuLanguageCompletionOptions {
 
 const Identifier = /^[\w$\xa1-\uffff][\w$\d\xa1-\uffff]*$/;
 
+const durationArguments = [
+  { label: "day", type: "variable" },
+  { label: "days", type: "variable" },
+  { label: "month", type: "variable" },
+  { label: "months", type: "variable" },
+  { label: "year", type: "variable" },
+  { label: "years", type: "variable" },
+];
+
 type NodeApproximation = { lastChild: NodeApproximation | null };
 function rightmostLeaf<NodeType extends NodeApproximation>(node: NodeType) {
   if (node.lastChild !== null) {
@@ -54,9 +63,15 @@ export function dentakuCompletions({
         Identifier.test(context.state.sliceDoc(inner.from, inner.to)));
 
     const treeCursor = tree.cursorAt(context.pos, -1);
+    const nodeName = treeCursor.node.type.name;
+    const nodeParentName = treeCursor.node.parent?.type.name;
+    const isDurationArg =
+      nodeName === "Duration" ||
+      (nodeParentName === "Duration" && !["(", ")"].includes(nodeName));
     const isOperator =
       context.state.sliceDoc(context.pos - 1, context.pos) === " " &&
       treeCursor.prev() &&
+      !isDurationArg &&
       (rightmostLeaf(treeCursor.node).type.is("Literal") ||
         rightmostLeaf(treeCursor.node).name === "VariableName");
 
@@ -71,9 +86,18 @@ export function dentakuCompletions({
       const functionEntries = builtInFunctions
         .map(makeEntryForBuiltInFunctions)
         .filter(Boolean);
+      const durationEntries = isDurationArg
+        ? durationArguments
+        : [{ label: "duration", type: "function" }];
 
       return {
-        options: [...functionEntries, ...variableEntries],
+        options: [...functionEntries, ...variableEntries, ...durationEntries],
+        from: isWord ? inner.from : context.pos,
+        validFor: Identifier,
+      };
+    } else if (isDurationArg) {
+      return {
+        options: durationArguments.filter(Boolean),
         from: isWord ? inner.from : context.pos,
         validFor: Identifier,
       };
